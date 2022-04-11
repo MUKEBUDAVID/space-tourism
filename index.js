417b08124652d8abddc6b0f4b6fca847e52c25ae
MUKEBUDAVID <davidmukebu@gmail.com>
2022-04-11 20:57:08 +0100

    premier commit du projet space

---
+++ index.js
@@ -0,0 +1,9 @@
+// const contenaire = document.getElementById("divine");
+// let tab=0
+
+// fetch("./data.json")
+//     .then((res) => res.json())
+//     .then((data)=>tab=data.destinations[0].description )
+//     .then((data)=>contenaire.textContent=`${tab}${tab}${tab}${tab}${tab}${tab}${tab}`)
+let date = new Date();
+console.log(date);
